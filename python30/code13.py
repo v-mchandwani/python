4a9920a405ed9ba1374db7d53d100e20afbb7b0b
v-mchandwani <mahima.chandwani@outlook.com>
2026-03-01 00:15:06 +0000

    added new files

---
+++ python30/code13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+num=int(input("enter number: "))
+if num%3==0 and num%5==0:
+    print("FizzBuzz")
+elif num%3==0:
+        print("Fizz")
+elif num%5==0:
+        print("Buzz")
+else:
+        print(num)
---
+++ python30/code14.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+num=input("Enter a min 2 digit number: ")
+total=sum(map(int,num))
+print(total)
+
+
